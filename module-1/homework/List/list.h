d0035e9e6a26d9fa6eabaea289a7a1bb21797315
bulumutka <mishasdk@gmail.com>
2020-10-03 22:54:44 +0300

    Implementation list added

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -1,20 +1,21 @@
-#pragma once
-#include <cstddef>
+#ifndef LIST_H_
+#define LIST_H_
 
+#include <cstddef>
+#include <stdexcept>
+#include <unordered_set>
+#include <map>
 
 namespace task {
 
-
 class list {
-
-public:
-
+  public:
     list();
     list(size_t count, const int& value = int());
+    list(const list& other);
 
     ~list();
     list& operator=(const list& other);
-
 
     int& front();
     const int& front() const;
@@ -22,11 +23,9 @@ public:
     int& back();
     const int& back() const;
 
-
     bool empty() const;
     size_t size() const;
     void clear();
-
 
     void push_back(const int& value);
     void pop_back();
@@ -35,17 +34,27 @@ public:
     void resize(size_t count);
     void swap(list& other);
 
-
     void remove(const int& value);
     void unique();
     void sort();
 
-    // Your code goes here?..
+    struct node {
+        node(const int value, node* next = nullptr);
 
-private:
+        int value;
+        node* next;
+    };
 
-    // Your code goes here...
+  private:
+    node* head_;
+    size_t size_;
 
+    void assertNotEmpty_() const;
+    bool equals_(const list& other) const;
+    void init_(const list& other);
 };
 
+
 }  // namespace task
+
+#endif
