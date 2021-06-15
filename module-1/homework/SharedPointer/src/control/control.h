d19b9a7e037d8838171df8068ff33eeb116ca2de
Mikhail Serdyukov <mishasdk@gmail.com>
2021-06-15 23:26:10 +0200

    Shared ptr added

--- module-1/homework/SharedPointer/src/control/control.h
+++ module-1/homework/SharedPointer/src/control/control.h
@@ -1,28 +1,68 @@
 #pragma once
 
 #include <atomic>
+#include <cassert>
 
 class SharedCount {
 public:
-    // Your code goes here...
+    virtual ~SharedCount() = default;
+
+    size_t Count() const {
+        return value_.load();
+    }
+
+    bool IsZero() const {
+        return Count() == 0;
+    }
+
+    void Add() {
+        ++value_;
+    }
+
+    virtual void Remove() {
+        assert(!IsZero());
+        --value_;
+    }
 
 protected:
-    // Your code goes here...
+    std::atomic<size_t> value_{0};
 };
 
 class SharedWeakCount : public SharedCount {
 public:
-    // Your code goes here...
+    bool IsZeroWeak() const {
+        return IsZero() && valueWeak_.load() == 0;
+    }
+
+    void AddWeak() {
+        valueWeak_++;
+    }
+
+    void RemoveWeak() {
+        assert(valueWeak_.load() != 0);
+        --valueWeak_;
+    }
 
 protected:
-    // Your code goes here...
+    std::atomic<size_t> valueWeak_{0};
 };
 
 template <typename T, typename Deleter>
 class ControlBlock : public SharedWeakCount {
 public:
-    // Your code goes here...
+    explicit ControlBlock(T* value) : value_(value) {
+        Add();
+    }
+
+    virtual void Remove() override {
+        SharedCount::Remove();
+        if (IsZero()) {
+            Deleter del;
+            del(value_);
+            value_ = nullptr;
+        }
+    }
 
 private:
-    // Your code goes here...
+    T* value_ = nullptr;
 };
